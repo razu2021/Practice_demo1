431748c5375b7b3c8aeb0993e9f00b774ad0d10d
razu2021 <mdrazuhossainraj@gmail.com>
2023-12-23 16:55:47 +0600

    projects

---
+++ assets/js/owl.js
@@ -0,0 +1,43 @@
+
+$(document).ready(function(){
+    $(".index_services").owlCarousel({
+        autoplay:true,
+        loop:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+  });
+// index service slider end here 
+$(document).ready(function(){
+    $(".index_client_testimonials").owlCarousel({
+        autoplay:true,
+        loop:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:2
+            }
+        }
+    });
+  });
+// index testimonials  slider end here 
+
+
+
+
+
+ 
